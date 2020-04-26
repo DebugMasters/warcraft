5d14707a0fce458e2eabadd60875b45b3f7a062a
zhengxiaotai <tony19920903@163.com>
2020-04-26 16:29:57 +0800

    展示名称

--- src/main/java/com/fenghuolun/modules/system/entity/NuanxinTradeCatalog.java
+++ src/main/java/com/fenghuolun/modules/system/entity/NuanxinTradeCatalog.java
@@ -15,14 +15,15 @@ import com.jeesite.common.mybatis.mapper.query.QueryType;
 /**
  * nuanxin_trade_catalogEntity
  * @author zhengxiaotai
- * @version 2020-04-07
+ * @version 2020-04-26
  */
 @Table(name="nuanxin_trade_catalog", alias="a", columns={
-		@Column(name="catalog_code", attrName="catalogCode", label="catalog_code", isPK=true),
-		@Column(name="catalog_name", attrName="catalogName", label="catalog_name", queryType=QueryType.LIKE, isTreeName=true),
-		@Column(name="catalog_type", attrName="catalogType", label="catalog_type"),
-		@Column(name="catalog_display_type", attrName="catalogDisplayType", label="catalog_display_type"),
-		@Column(name="money", attrName="money", label="money"),
+		@Column(name="catalog_code", attrName="catalogCode", label="类目ID", isPK=true),
+		@Column(name="catalog_name", attrName="catalogName", label="类目名称", queryType=QueryType.LIKE, isTreeName=true),
+		@Column(name="catalog_type", attrName="catalogType", label="服务器类型"),
+		@Column(name="catalog_display_type", attrName="catalogDisplayType", label="展示类型", isQuery=false),
+		@Column(name="catalog_display_name", attrName="catalogDisplayName", label="展示标题", isQuery=false),
+		@Column(name="money", attrName="money", label="基础金额"),
 		@Column(includeEntity=TreeEntity.class),
 		@Column(name="status", attrName="status", label="status", isUpdate=false),
 		@Column(name="update_by", attrName="updateBy", label="更新者", isQuery=false),
@@ -32,11 +33,12 @@ import com.jeesite.common.mybatis.mapper.query.QueryType;
 public class NuanxinTradeCatalog extends TreeEntity<NuanxinTradeCatalog> {
 	
 	private static final long serialVersionUID = 1L;
-	private String catalogCode;		// catalog_code
-	private String catalogName;		// catalog_name
-	private Integer catalogType;		// catalog_type
-	private Integer catalogDisplayType;	// 展示类型
-	private Double money;		// money
+	private String catalogCode;		// 类目ID
+	private String catalogName;		// 类目名称
+	private Integer catalogType;		// 服务器类型
+	private Integer catalogDisplayType;		// 展示类型
+	private String catalogDisplayName;		// 展示标题
+	private Double money;		// 基础金额
 	
 	public NuanxinTradeCatalog() {
 		this(null);
@@ -64,8 +66,8 @@ public class NuanxinTradeCatalog extends TreeEntity<NuanxinTradeCatalog> {
 		this.catalogCode = catalogCode;
 	}
 	
-	@NotBlank(message="catalog_name不能为空")
-	@Length(min=0, max=30, message="catalog_name长度不能超过 30 个字符")
+	@NotBlank(message="类目名称不能为空")
+	@Length(min=0, max=36, message="类目名称长度不能超过 36 个字符")
 	public String getCatalogName() {
 		return catalogName;
 	}
@@ -74,7 +76,7 @@ public class NuanxinTradeCatalog extends TreeEntity<NuanxinTradeCatalog> {
 		this.catalogName = catalogName;
 	}
 	
-	@NotNull(message="catalog_type不能为空")
+	@NotNull(message="服务器类型不能为空")
 	public Integer getCatalogType() {
 		return catalogType;
 	}
@@ -86,9 +88,18 @@ public class NuanxinTradeCatalog extends TreeEntity<NuanxinTradeCatalog> {
 	public Integer getCatalogDisplayType() {
 		return catalogDisplayType;
 	}
-	
+
 	public void setCatalogDisplayType(Integer catalogDisplayType) {
 		this.catalogDisplayType = catalogDisplayType;
+	}
+	
+	@Length(min=0, max=20, message="展示标题长度不能超过 20 个字符")
+	public String getCatalogDisplayName() {
+		return catalogDisplayName;
+	}
+
+	public void setCatalogDisplayName(String catalogDisplayName) {
+		this.catalogDisplayName = catalogDisplayName;
 	}
 	
 	public Double getMoney() {
