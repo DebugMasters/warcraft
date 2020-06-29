a860e9e50bafa1b291509d074c4329a0e2306092
tony1 <tony1@DESKTOP-ABSK440>
2020-06-29 23:09:35 +0800

    AES解密功能

--- src/main/java/com/fenghuolun/modules/system/web/NuanxinDesktopController.java
+++ src/main/java/com/fenghuolun/modules/system/web/NuanxinDesktopController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.fenghuolun.modules.order.entity.NuanxinOrder;
 import com.fenghuolun.modules.order.service.NuanxinOrderService;
 import com.fenghuolun.modules.order.service.NuanxinWechatOrderService;
+import com.fenghuolun.modules.system.service.NuanxinConfigService;
 import com.fenghuolun.modules.user.entity.NuanxinUser;
 import com.fenghuolun.modules.user.service.NuanxinUserService;
 import com.github.abel533.echarts.Option;
@@ -31,6 +32,8 @@ import com.github.abel533.echarts.json.GsonUtil;
 import com.github.abel533.echarts.series.Line;
 import com.github.abel533.echarts.series.Pie;
 import com.jeesite.common.web.BaseController;
+import com.jeesite.modules.sys.entity.User;
+import com.jeesite.modules.sys.utils.UserUtils;
 
 /**
  * 后台主页相关接口
@@ -47,6 +50,8 @@ public class NuanxinDesktopController extends BaseController {
 	private NuanxinOrderService nuanxinOrderService;
 	@Autowired
 	private NuanxinWechatOrderService nuanxinWechatOrderService;
+	@Autowired
+	private NuanxinConfigService nuanxinConfigService;
 
 	@ResponseBody
 	@PostMapping(value = "cardData")
@@ -121,6 +126,26 @@ public class NuanxinDesktopController extends BaseController {
 		option.yAxis(yAxis);
 		option.series(line);
 		result.put("data", GsonUtil.format(option));
+		return result;
+	}
+	
+	/*
+	 * 加密key和iv
+	 */
+	@ResponseBody
+	@RequestMapping(value = "getDecryptInfo")
+	public Map<String, Object> getDecryptInfo(HttpServletRequest request, HttpServletResponse response) {
+		Map<String, Object> result = new HashMap<>();
+        User user = UserUtils.getUser();
+        if (user == null || user.getId() == null) {
+        	result.put("success", false);
+			result.put("msg", "权限不足");
+			return result;
+        }
+		result.put("key", nuanxinConfigService.getByConfigKey("nx.decrypt.key").getConfigValue());
+		result.put("iv", nuanxinConfigService.getByConfigKey("nx.decrypt.iv").getConfigValue());
+    	result.put("success", true);
+		result.put("msg", "成功");
 		return result;
 	}
 }
